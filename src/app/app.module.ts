a417dc8911d3598203a5cacd9c17aba50a3ba506
Magali Bautista-Lee <magzb.99@gmail.com>
2021-02-20 15:38:03 -0500

    Adds ,tailwind, Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginModule } from './views/user-login/user-login.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UserLoginModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserLoginModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
