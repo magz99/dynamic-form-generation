0f6c161a079c106082ef37844990be9a9e337487
Magali Bautista-Lee <magzb.99@gmail.com>
2021-02-20 17:03:41 -0500

    Feature: Updates login, adds some styling

--- src/app/views/user-login/user-login.module.ts
+++ src/app/views/user-login/user-login.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   imports: [
     SharedModule,
     UserLoginRoutingModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
   ],
   declarations: [UserLoginComponent],
 })
