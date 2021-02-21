d9c5f19edbbaaf040c64a6b30bc3e5b9742a9097
magz99 <magzb.99@gmail.com>
2021-02-20 20:02:08 -0500

    Styling and form updates

--- src/app/views/user-login/user-login.component.ts
+++ src/app/views/user-login/user-login.component.ts
@@ -9,7 +9,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class UserLoginComponent {
   loginForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required]),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('', Validators.required),
   });
   constructor() {}
+
+  onLoginSubmit(): void {
+    // console.log('username and pw: %s %s', this.loginForm.controls.username.value, this.loginForm.controls.password.value);
+    if (this.loginForm.invalid) {
+      console.error('the form is not valid');
+    } else {
+      console.log('The form is valid');
+    }
+  }
 }
