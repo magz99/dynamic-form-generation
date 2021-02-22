3d5cc7bf9256f56e6115ee1bc270612069dca56e
Magali Bautista-Lee <magzb.99@gmail.com>
2021-02-21 21:47:07 -0500

    Adds user registration skeleton

---
+++ packages/frontend/src/app/views/user-registration/user-registration.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRegistrationComponent } from './user-registration.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UserRegistrationComponent]
+})
+export class UserRegistrationModule { }
